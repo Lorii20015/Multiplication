b03f7f90808849c86e597b2e1fe7a6787c7b9754
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:47:02 +0100

    Mhmmm

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            string x = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            string y = Console.ReadLine();
+            int a = Int32.Parse(x);
+            int b = Int32.Parse(y);
+            int z = a * b;
+            Console.WriteLine(z);
+        }
+    }
+}
